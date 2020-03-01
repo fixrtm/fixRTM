317c781fcca7e8c2fdd7128fc089fff993c7280a
anatawa12 <anatawa12@icloud.com>
2020-03-01 12:57:21 +0900

    download latest

--- buildSrc/src/main/kotlin/DownloadModJar.kt
+++ buildSrc/src/main/kotlin/DownloadModJar.kt
@@ -9,10 +9,8 @@ import java.io.File
 import kotlin.properties.Delegates
 
 open class DownloadModJar : DefaultTask() {
-    @get:Input
     var projectId: Int by Delegates.notNull()
 
-    @get:OutputFile
     lateinit var to: File
 
     @TaskAction
@@ -21,7 +19,14 @@ open class DownloadModJar : DefaultTask() {
         if (!files.isPresent)
             error("project#$projectId files not found")
 
-        val url = files.get().last().downloadURL()
+        val file = files.get()
+                .filterNot { "1.12.2" !in it.gameVersionStrings() }
+                .maxBy { getVersionString(it.displayName()) }
+                ?: error("project#$projectId files not found")
+
+        println("file name is: ${file.displayName()}, id: ${file.id()}")
+
+        val url = file.downloadURL()
 
         val response = OkHttpClient().newCall(Request.Builder().url(url).get().build()).execute()
 
@@ -30,5 +35,27 @@ open class DownloadModJar : DefaultTask() {
 
         to.parentFile.mkdirs()
         response.body()!!.byteStream().copyTo(to.outputStream())
+    }
+
+    private fun getVersionString(name: String): Version {
+        val regex = """.*?([0-9]+(\.[0-9]+)+).*""".toRegex()
+        val match = regex.matchEntire(name)
+                ?: throw NullPointerException("Expression '$regex.matchEntire($name)' must not be null")
+        return Version(match.groupValues[1])
+    }
+
+}
+
+private inline class Version(val version: String) : Comparable<Version> {
+    override fun compareTo(other: Version): Int {
+        val a = version.split('.')
+        val b = version.split('.')
+
+        for (i in 0 until minOf(a.size, b.size)) {
+            val res = a[i].toInt().compareTo(b[i].toInt())
+            if (res != 0) return res
+        }
+
+        return 0
     }
 }
