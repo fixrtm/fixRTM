cbc91dd9450de1a71fe44a3dfc9cb82be8037c36
anatawa12 <anatawa12@icloud.com>
2020-03-14 21:12:46 +0900

    デコンパイラをjd-coreからfernflowerに変更

--- buildSrc/src/main/kotlin/com/anatawa12/fixrtm/gradle/decompileJar.kt
+++ buildSrc/src/main/kotlin/com/anatawa12/fixrtm/gradle/decompileJar.kt
@@ -1,6 +1,8 @@
 package com.anatawa12.fixrtm.gradle
 
-import org.jd.core.v1.ClassFileToJavaSourceDecompiler
+import org.jetbrains.java.decompiler.main.decompiler.ConsoleDecompiler
+import org.jetbrains.java.decompiler.main.extern.IFernflowerLogger
+import org.slf4j.LoggerFactory
 import java.io.File
 
 fun main() {
@@ -10,18 +12,40 @@ fun main() {
     )
 }
 
+class ConsoleDecompilerImpl(destination: File, options: Map<String, Any>, logger: IFernflowerLogger)
+    : ConsoleDecompiler(destination, options, logger) {
+}
+
+object FernflowerLogger : IFernflowerLogger() {
+    val slf4jLogger = LoggerFactory.getLogger("decompile-jar")
+
+    override fun writeMessage(message: String, severity: Severity) {
+        when (severity) {
+            Severity.TRACE -> slf4jLogger.trace(message)
+            Severity.INFO -> slf4jLogger.info(message)
+            Severity.WARN -> slf4jLogger.warn(message)
+            Severity.ERROR -> slf4jLogger.error(message)
+        }
+    }
+
+    override fun writeMessage(message: String, severity: Severity, throwable: Throwable?) {
+        when (severity) {
+            Severity.TRACE -> slf4jLogger.trace(message, throwable)
+            Severity.INFO -> slf4jLogger.info(message, throwable)
+            Severity.WARN -> slf4jLogger.warn(message, throwable)
+            Severity.ERROR -> slf4jLogger.error(message, throwable)
+        }
+    }
+
+}
+
 fun decompileJar(jarFile: File, outputDir: File) {
     outputDir.mkdirs()
-    val loader = JarLoader(jarFile)
-    val decompiler = ClassFileToJavaSourceDecompiler()
-    for (className in loader.getAllClassName()) {
-        if (className.contains('$')) continue
-        val printer = JdPrinter()
+    val logger = FernflowerLogger
 
-        decompiler.decompile(loader, printer, className)
+    val decompiler = ConsoleDecompilerImpl(outputDir, mapOf(), logger)
 
-        outputDir.resolve("$className.java")
-                .apply { parentFile.mkdirs() }
-                .writeText(printer.toString())
-    }
+    decompiler.addSource(jarFile)
+
+    decompiler.decompileContext()
 }
