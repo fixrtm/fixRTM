075ee7bc76b9c9aa36505bd0443b17aceca707fb
anatawa12 <anatawa12@icloud.com>
2020-03-17 18:18:16 +0900

    stash

--- apply_patch.sh
+++ apply_patch.sh
@@ -3,8 +3,8 @@ function apply_patch() {
   local src_dir dst_dir
   src_dir="mods/$1.deobf.jar.src"
   processed_dir="mods/$1.deobf.jar.src.processed"
-  dst_dir="src/api/$1"
-  rm -rf "src/api/$1"
+  dst_dir="src/main/$1"
+  rm -rf "$dst_dir"
   (cd "$src_dir" && find . -type f) | while read -r path; do
     ./process_non_patch.sh "$src_dir/$path" "$processed_dir/$path"
   done
--- create_diff.sh
+++ create_diff.sh
@@ -1,7 +1,7 @@
 function make_patch() {
   src_dir="mods/$1.deobf.jar.src"
   processed_dir="mods/$1.deobf.jar.src.processed"
-  dst_dir="src/api/$1"
+  dst_dir="src/main/$1"
   (cd "$src_dir" && find . -type f) | while read -r path; do
     ./process_non_patch.sh "$src_dir/$path" "$processed_dir/$path"
   done
