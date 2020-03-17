57f0bed993395de3aabcc1dccf9fea67f294e349
anatawa12 <anatawa12@icloud.com>
2020-03-17 13:17:48 +0900

    fix: this;のエラー

--- apply_patch.sh
+++ apply_patch.sh
@@ -1,7 +1,19 @@
+# $1: source file
+# $2: dst file
+function processNonPatch() {
+    mkdir -p "$(dirname "$2")"
+    cat "$1" | grep -v "this;" > "$2"
+}
+
 function apply_patch() {
+  local src_dir dst_dir
+  src_dir="mods/$1.deobf.jar.src"
+  dst_dir="src/api/$1"
   rm -rf "src/api/$1"
-  mkdir "src/api/"
-  cp -rf "mods/$1.deobf.jar.src" "src/api/$1"
+  (cd "$src_dir" && find . -type f) | while read -r path; do
+    processNonPatch "$src_dir/$path" "$dst_dir/$path"
+  done
+  #cp -rf "mods/$1.deobf.jar.src" "src/api/$1"
   patch -p1 < "patches/$1.patch"
 }
 
