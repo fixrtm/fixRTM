7bffd8e0c624fc88935b0cfc0442bb18ed1aa22c
anatawa12 <anatawa12@icloud.com>
2020-03-17 11:55:22 +0900

    デコンパイル時にライブラリを読ませるように

--- buildSrc/src/main/kotlin/DecompileJar.kt
+++ buildSrc/src/main/kotlin/DecompileJar.kt
@@ -1,6 +1,7 @@
 import com.anatawa12.fixrtm.gradle.decompileJar
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 import java.io.File
@@ -8,12 +9,13 @@ import java.io.File
 open class DecompileJar : DefaultTask() {
     @InputFile var jarFile: File? = null
     @OutputDirectory var sourceOutputTo: File? = null
+    @InputFiles var libraries: Iterable<File> = listOf()
 
     @TaskAction
     fun download() {
         jarFile ?: error("jarFile not initialized") // check init
         sourceOutputTo ?: error("sourceOutputTo not initialized") // check init
 
-        decompileJar(jarFile!!, sourceOutputTo!!)
+        decompileJar(jarFile!!, sourceOutputTo!!, libraries)
     }
 }
--- buildSrc/src/main/kotlin/com/anatawa12/fixrtm/gradle/decompileJar.kt
+++ buildSrc/src/main/kotlin/com/anatawa12/fixrtm/gradle/decompileJar.kt
@@ -3,6 +3,7 @@ package com.anatawa12.fixrtm.gradle
 import org.jetbrains.java.decompiler.main.DecompilerContext
 import org.jetbrains.java.decompiler.main.decompiler.ConsoleDecompiler
 import org.jetbrains.java.decompiler.main.extern.IFernflowerLogger
+import org.jetbrains.java.decompiler.main.extern.IFernflowerPreferences
 import org.slf4j.LoggerFactory
 import java.io.File
 import java.io.IOException
@@ -11,7 +12,8 @@ import java.util.jar.Manifest
 fun main() {
     decompileJar(
             jarFile = File("./mods/rtm.jar"),
-            outputDir = File("./mods/rtm.jar.classes/")
+            outputDir = File("./mods/rtm.jar.classes/"),
+            libraries = listOf()
     )
 }
 
@@ -63,16 +65,27 @@ object FernflowerLogger : IFernflowerLogger() {
 
 }
 
-fun decompileJar(jarFile: File, outputDir: File) {
+fun decompileJar(jarFile: File, outputDir: File, libraries: Iterable<File>) {
     outputDir.mkdirs()
     val logger = FernflowerLogger
 
     val decompiler = ConsoleDecompilerImpl(outputDir, mapOf(
-            "dgs" to "1",
-            "rsy" to "1"
-    ), logger)
+            IFernflowerPreferences.HIDE_DEFAULT_CONSTRUCTOR to "0",
+            IFernflowerPreferences.DECOMPILE_GENERIC_SIGNATURES to "1",
+            IFernflowerPreferences.REMOVE_SYNTHETIC to "1",
+            IFernflowerPreferences.REMOVE_BRIDGE to "1",
+            IFernflowerPreferences.LITERALS_AS_IS to "1",
+            IFernflowerPreferences.NEW_LINE_SEPARATOR to "1",
+            IFernflowerPreferences.MAX_PROCESSING_METHOD to 60,
+            IFernflowerPreferences.INDENT_STRING to "    ",
+            IFernflowerPreferences.UNIT_TEST_MODE to "0")
+            , logger)
 
     decompiler.addSource(jarFile)
+
+    for (library in libraries) {
+        decompiler.addLibrary(library)
+    }
 
     decompiler.decompileContext()
 }
