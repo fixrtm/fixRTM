ec0ea020c85746c189c3642799b50f9811cfd540
anatawa12 <anatawa12@icloud.com>
2020-03-29 19:11:15 +0900

    remove DummyVisitors.kt

--- src/main/java/com/anatawa12/fixRtm/asm/DummyVisitors.kt
+++
@@ -1,7 +0,0 @@
-package com.anatawa12.fixRtm.asm
-
-import org.objectweb.asm.MethodVisitor
-import org.objectweb.asm.Opcodes
-import org.objectweb.asm.Opcodes.ASM5
-
-object DummyMethodVisitor : MethodVisitor(ASM5)
