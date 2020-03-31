99a7d15a00da27f3af0a5e1bb2d93f29946adc56
anatawa12 <anatawa12@icloud.com>
2020-03-31 14:41:38 +0900

    fix: cant start on server

--- src/main/java/com/anatawa12/fixRtm/asm/FixRtmCorePlugin.kt
+++ src/main/java/com/anatawa12/fixRtm/asm/FixRtmCorePlugin.kt
@@ -1,7 +1,12 @@
 package com.anatawa12.fixRtm.asm
 
 import net.minecraft.launchwrapper.Launch
+import net.minecraftforge.fml.relauncher.FMLLaunchHandler
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin
+import net.minecraftforge.fml.relauncher.Side
+import java.io.File
+import java.net.URL
+import java.net.URLClassLoader
 
 
 @IFMLLoadingPlugin.TransformerExclusions(
@@ -14,6 +19,20 @@ import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin
 )
 class FixRtmCorePlugin : IFMLLoadingPlugin {
     init {
+        if (FMLLaunchHandler.side() == Side.SERVER) {
+            val url = "https://repo1.maven.org/maven2/org/apache/commons/commons-compress/1.9/commons-compress-1.9.jar"
+            val file = File("./mods/fixrtm-deps/${url.substringAfterLast('/')}").apply { parentFile.mkdirs() }
+            if (!file.exists())
+                URL(url).openStream().use { uis ->
+                    file.outputStream().buffered().use { fos ->
+                        uis.copyTo(fos)
+                    }
+                }
+            val addURL = URLClassLoader::class.java.getDeclaredMethod("addURL", URL::class.java)
+            addURL.isAccessible = true
+            addURL.invoke(Launch.classLoader.javaClass.classLoader, file.toURI().toURL())
+            Launch.classLoader.addURL(file.toURI().toURL())
+        }
         Launch.classLoader.registerTransformer("com.anatawa12.fixRtm.asm.PatchApplier")
     }
 
