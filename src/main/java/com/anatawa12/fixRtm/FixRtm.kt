2de628c308bff7b2034a7aba50c5c8dc0a900daa
anatawa12 <anatawa12@icloud.com>
2020-04-18 16:42:14 +0900

    テクスチャのジェネレータを追加

--- src/main/java/com/anatawa12/fixRtm/FixRtm.kt
+++ src/main/java/com/anatawa12/fixRtm/FixRtm.kt
@@ -1,30 +1,41 @@
 package com.anatawa12.fixRtm
 
-import com.anatawa12.fixRtm.rtm.modelpack.modelset.dummies.*
 import com.anatawa12.fixRtm.network.NetworkHandler
+import com.anatawa12.fixRtm.rtm.modelpack.modelset.dummies.*
 import jp.ngt.ngtlib.NGTCore
 import jp.ngt.rtm.RTMCore
 import net.minecraft.block.Block
 import net.minecraft.client.Minecraft
 import net.minecraft.client.resources.IReloadableResourceManager
 import net.minecraft.item.Item
+import net.minecraft.util.ResourceLocation
 import net.minecraftforge.client.event.ModelRegistryEvent
 import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.event.RegistryEvent
 import net.minecraftforge.fml.client.registry.ClientRegistry
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.ModMetadata
+import net.minecraftforge.fml.common.event.FMLConstructionEvent
 import net.minecraftforge.fml.common.event.FMLInitializationEvent
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.fml.common.network.NetworkCheckHandler
 import net.minecraftforge.fml.relauncher.Side
+import java.awt.Color
+import java.awt.image.BufferedImage
+import javax.imageio.ImageIO
+import kotlin.math.max
 
 @Mod(modid = FixRtm.MODID, dependencies = "required:${RTMCore.MODID}@${RTMCore.VERSION};required:${NGTCore.MODID}@${NGTCore.VERSION};")
 object FixRtm {
     const val MODID = "fix-rtm"
     lateinit var modMetadata: ModMetadata
             private set
+
+    @Mod.EventHandler
+    fun construct(e: FMLConstructionEvent) {
+        if (e.side == Side.CLIENT) registerGenerators()
+    }
 
     @Mod.EventHandler
     fun preInit(e: FMLPreInitializationEvent) {
@@ -65,12 +76,42 @@ object FixRtm {
     @SubscribeEvent
     fun registerModel(e: ModelRegistryEvent) {
         ClientRegistry.registerTileEntity(TestTileEntity::class.java, "test", TestSPRenderer)
+        DummyModelObject.init()
+    }
+
+
+    fun registerGenerators() {
         Minecraft.getMinecraft().defaultResourcePacks.add(GeneratedResourcePack)
         Minecraft.getMinecraft().resourceManager
                 .let { it as IReloadableResourceManager }
                 .registerReloadListener(GeneratedResourcePack)
-        DummyModelObject.init()
+
+        GeneratedResourcePack.addImageGenerator("textures/generated/items/item_test_train.png") {
+            try {
+                val baseBackImage = Minecraft.getMinecraft().resourceManager
+                        .getResource(ResourceLocation("rtm:textures/items/item_ec.png"))
+                        .inputStream.let { ImageIO.read(it) }
+                val testTrainText = Minecraft.getMinecraft().resourceManager
+                        .getResource(ResourceLocation(MODID, "textures/template/test_train_text.png"))
+                        .inputStream.let { ImageIO.read(it) }
+                val img = BufferedImage(
+                        max(baseBackImage.width, testTrainText.width),
+                        max(baseBackImage.height, testTrainText.height),
+                        BufferedImage.TYPE_INT_ARGB)
+
+                val graphics = img.createGraphics()
+
+                graphics.drawImage(baseBackImage, 0, 0, img.width, img.height, Color(0, 0, 0, 0), null)
+                graphics.drawImage(testTrainText, 0, 0, img.width, img.height, Color(0, 0, 0, 0), null)
+
+                img
+            } catch (e: Throwable) {
+                throw e
+            }
+        }
     }
+    //assets/rtm/models/item/item_train_127.json
+    //                       item_train_fixrtm_test
 
     var serverHasFixRTM = true
 
