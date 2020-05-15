6020feb485cbe335a2643edb6cd3db0897f55912
anatawa12 <anatawa12@icloud.com>
2020-05-15 21:12:15 +0900

    バージョンアップ時に注視すべき情報をgitに残す機構を追加

---
+++ buildSrc/src/main/kotlin/PrintMethodCode.kt
@@ -0,0 +1,41 @@
+import com.anatawa12.fixrtm.gradle.classHierarchy.ClassHierarchy
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.FileTree
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.TaskAction
+import org.objectweb.asm.util.Textifier
+import org.objectweb.asm.util.TraceClassVisitor
+import java.io.File
+import java.io.PrintWriter
+
+open class PrintMethodCode : DefaultTask() {
+    @InputFiles
+    var files: FileTree = project.files().asFileTree
+    @OutputFile
+    var outTo: File? = null
+    var ofClass: String? = null
+    var methodName: String? = null
+    var methodDesc: String? = null
+
+    @TaskAction
+    fun run() {
+        val classes = ClassHierarchy().apply { load(files) }
+        val outTo = outTo ?: error("outTo not inited")
+        val ofClass = ofClass ?: error("ofClass not inited")
+        val methodName = methodName ?: error("methodName not inited")
+        val methodDesc = methodDesc ?: error("methodDesc not inited")
+
+        val theClass = classes.getByInternalName(ofClass.replace('.', '/'))
+        if (!theClass.isLoaded)
+            throw IllegalStateException("ofClass is not loaded")
+
+        val method = theClass.methods.singleOrNull { it.name == methodName && it.desc == methodDesc }
+                ?: throw IllegalStateException("method is not loaded")
+
+        val node = method.node
+        val printer = Textifier()
+        node.accept(TraceClassVisitor(null, printer, null))
+        PrintWriter(outTo.writer()).use { printer.print(it) }
+    }
+}
---
+++ buildSrc/src/main/kotlin/PrintStaticFields.kt
@@ -0,0 +1,37 @@
+import com.anatawa12.fixrtm.gradle.classHierarchy.ClassHierarchy
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.FileTree
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.TaskAction
+import java.io.File
+
+open class PrintStaticFields : DefaultTask() {
+    @InputFiles
+    var files: FileTree = project.files().asFileTree
+    @OutputFile
+    var outTo: File? = null
+    var ofClass: String? = null
+
+    @TaskAction
+    fun run() {
+        val classes = ClassHierarchy().apply { load(files) }
+        val outTo = outTo ?: error("outTo not inited")
+        val ofClass = ofClass ?: error("ofClass not inited")
+
+        val theClass = classes.getByInternalName(ofClass.replace('.', '/'))
+        if (!theClass.isLoaded)
+            throw IllegalStateException("ofClass is not loaded")
+
+        val file = buildString {
+            for (field in theClass.fields.filter { it.isStatic }) {
+                appendln("name: ${field.name}")
+                appendln("type: ${field.type}")
+                appendln()
+            }
+        }
+
+        outTo.apply { parentFile.mkdirs() }
+                .writeText(file)
+    }
+}
