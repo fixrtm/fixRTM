3c9b78688ae54ab072fe24deaaa2bdc57c4cfb47
anatawa12 <anatawa12@icloud.com>
2020-05-19 09:48:35 +0900

    configにcachedScriptsを追加

--- src/main/java/com/anatawa12/fixRtm/FixRtm.kt
+++ src/main/java/com/anatawa12/fixRtm/FixRtm.kt
@@ -6,6 +6,7 @@ import com.anatawa12.fixRtm.io.FIXFileLoader
 import com.anatawa12.fixRtm.network.NetworkHandler
 import com.anatawa12.fixRtm.ngtlib.renderer.model.CachedPolygonModel
 import com.anatawa12.fixRtm.rtm.modelpack.modelset.dummies.*
+import com.anatawa12.fixRtm.scripting.ExecutedScriptCache
 import com.anatawa12.fixRtm.scripting.RhinoHooks
 import jp.ngt.ngtlib.NGTCore
 import jp.ngt.rtm.RTMCore
@@ -46,6 +47,7 @@ object FixRtm {
         RhinoHooks// load
         FIXFileLoader // init
         if (!MainConfig.cachedPolygonModel) CachedPolygonModel // init
+        if (!MainConfig.cachedScripts) ExecutedScriptCache // init
         if (e.side == Side.CLIENT) registerGenerators()
     }
 
