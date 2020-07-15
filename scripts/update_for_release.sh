586c6f17423a21471768ca7db08a1aa92156acd7
anatawa12 <anatawa12@icloud.com>
2020-07-15 23:09:43 +0900

    fix script

--- scripts/update_for_release.sh
+++ scripts/update_for_release.sh
@@ -1,9 +1,9 @@
 # バージョン情報を作成
 if [ "$VERSION_NAME_IN" == "snapshot-generated" ]; then
-    version_name_result="$(date "+SNAPSHOT-%Y-%m-%d-%H-%M-%S")"
+    version_name="$(date "+SNAPSHOT-%Y-%m-%d-%H-%M-%S")"
     prerelease=true
 else
-    version_name_result="$VERSION_NAME_IN"
+    version_name="$VERSION_NAME_IN"
     prerelease=false
 fi
 
@@ -24,21 +24,26 @@ if [ "$prerelease" != "true" ]; then
 
   # masterの行を複製してmasterをバージョン名で置き換える
   cat version-map.md > "$tmp_file"
-  awk '{ if (match($0, "master")) { printf("%s%-14s |\n", substr($0, 0, 33), "'"$version_name_result"'") } ; print }' "$tmp_file" > version-map.md
+  awk '{ if (match($0, "master")) { printf("%s%-14s |\n", substr($0, 0, 33), "'"$version_name"'") } ; print }' "$tmp_file" > version-map.md
   rm "$tmp_file"
 fi
 
 # gradle.properties を更新
 cat gradle.properties > "$tmp_file"
-sed 's/^modVersion=.*/modVersion='"$version_name_result"'/' "$tmp_file" > gradle.properties
+sed 's/^modVersion=.*/modVersion='"$version_name"'/' "$tmp_file" > gradle.properties
 rm "$tmp_file"
+
+# コミット
+
+git commit -am "$version_name"
+git push
 
 # 出力設定
 
-asset_path="./build/libs/fixRtm-$version_name_result.jar"
-asset_name="fixRtm-$version_name_result.jar"
+asset_path="./build/libs/fixRtm-$version_name.jar"
+asset_name="fixRtm-$version_name.jar"
 
-echo "::set-output version_name=$version_name_result"
+echo "::set-output version_name=$version_name"
 echo "::set-output prerelease=$prerelease"
 echo "::set-output asset_path=$asset_path"
 echo "::set-output asset_name=$asset_name"
