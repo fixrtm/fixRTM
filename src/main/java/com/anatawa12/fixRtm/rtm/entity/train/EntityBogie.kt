f57d254ead58ff39359ddb7592dca9528bb0f720
anatawa12 <anatawa12@icloud.com>
2021-02-25 10:35:03 +0900

    notify when vehicle or bogie was unloaded

--- src/main/java/com/anatawa12/fixRtm/rtm/entity/train/EntityBogie.kt
+++ src/main/java/com/anatawa12/fixRtm/rtm/entity/train/EntityBogie.kt
@@ -1,8 +1,12 @@
 package com.anatawa12.fixRtm.rtm.entity.train
 
+import com.anatawa12.fixRtm.FixRtm
+import com.anatawa12.fixRtm.network.NetworkHandler
+import com.anatawa12.fixRtm.network.NotifyUntracked
 import jp.ngt.rtm.entity.train.EntityBogie
 
-@Suppress("EXTENSION_SHADOWED_BY_MEMBER")
+@Suppress("EXTENSION_SHADOWED_BY_MEMBER", "unused")
 fun EntityBogie.onRemovedFromWorld() {
-    println("EntityBogie#onRemovedFromWorld: ${world.isRemote}")
+    if (FixRtm.serverHasFixRTM)
+        NetworkHandler.sendPacketServer(NotifyUntracked(entityId))
 }
--- src/main/java/com/anatawa12/fixRtm/rtm/entity/vehicle/EntityVehicleBase.kt
+++ src/main/java/com/anatawa12/fixRtm/rtm/entity/vehicle/EntityVehicleBase.kt
@@ -1,8 +1,12 @@
 package com.anatawa12.fixRtm.rtm.entity.vehicle
 
+import com.anatawa12.fixRtm.FixRtm
+import com.anatawa12.fixRtm.network.NetworkHandler
+import com.anatawa12.fixRtm.network.NotifyUntracked
 import jp.ngt.rtm.entity.vehicle.EntityVehicleBase
 
-@Suppress("EXTENSION_SHADOWED_BY_MEMBER")
+@Suppress("EXTENSION_SHADOWED_BY_MEMBER", "unused")
 fun EntityVehicleBase<*>.onRemovedFromWorld() {
-    println("EntityVehicleBase#onRemovedFromWorld: ${world.isRemote}")
+    if (FixRtm.serverHasFixRTM)
+        NetworkHandler.sendPacketServer(NotifyUntracked(entityId))
 }
