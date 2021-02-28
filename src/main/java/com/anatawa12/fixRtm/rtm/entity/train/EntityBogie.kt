00c1e931f7dd6ae351472a7f1ec24b5f78ed12a8
anatawa12 <anatawa12@icloud.com>
2021-02-28 21:43:41 +0900

    add small information about ModelSet for vehicles

--- src/main/java/com/anatawa12/fixRtm/rtm/entity/train/EntityBogie.kt
+++ src/main/java/com/anatawa12/fixRtm/rtm/entity/train/EntityBogie.kt
@@ -4,9 +4,21 @@ import com.anatawa12.fixRtm.FixRtm
 import com.anatawa12.fixRtm.network.NetworkHandler
 import com.anatawa12.fixRtm.network.NotifyUntracked
 import jp.ngt.rtm.entity.train.EntityBogie
+import net.minecraft.crash.CrashReportCategory
 
 @Suppress("EXTENSION_SHADOWED_BY_MEMBER", "unused")
 fun EntityBogie.onRemovedFromWorld() {
     if (FixRtm.serverHasFixRTM)
         NetworkHandler.sendPacketServer(NotifyUntracked(entityId))
+}
+
+@Suppress("EXTENSION_SHADOWED_BY_MEMBER", "unused")
+fun EntityBogie.addEntityCrashInfo(category: CrashReportCategory) = try {
+    val cfg = train?.resourceState?.resourceSet?.config
+    category.addCrashSection("Parent Train ModelSet Name",
+        cfg?.name ?: "no train on client")
+    category.addCrashSection("Parent Train ModelSet Source JSON Path",
+        cfg?.run { file ?: "no source" } ?: "no train on client")
+} catch (t: Throwable) {
+    category.addCrashSectionThrowable("Error Getting ModelSet", t)
 }
--- src/main/java/com/anatawa12/fixRtm/rtm/entity/vehicle/EntityVehicleBase.kt
+++ src/main/java/com/anatawa12/fixRtm/rtm/entity/vehicle/EntityVehicleBase.kt
@@ -4,9 +4,19 @@ import com.anatawa12.fixRtm.FixRtm
 import com.anatawa12.fixRtm.network.NetworkHandler
 import com.anatawa12.fixRtm.network.NotifyUntracked
 import jp.ngt.rtm.entity.vehicle.EntityVehicleBase
+import net.minecraft.crash.CrashReportCategory
 
 @Suppress("EXTENSION_SHADOWED_BY_MEMBER", "unused")
 fun EntityVehicleBase<*>.onRemovedFromWorld() {
     if (FixRtm.serverHasFixRTM)
         NetworkHandler.sendPacketServer(NotifyUntracked(entityId))
+}
+
+@Suppress("EXTENSION_SHADOWED_BY_MEMBER", "unused")
+fun EntityVehicleBase<*>.addEntityCrashInfo(category: CrashReportCategory) = try {
+    val cfg = resourceState.resourceSet.config
+    category.addCrashSection("ModelSet Name", cfg.name)
+    category.addCrashSection("ModelSet Source JSON Path", cfg.file ?: "no source")
+} catch (t: Throwable) {
+    category.addCrashSectionThrowable("Error Getting ModelSet", t)
 }
