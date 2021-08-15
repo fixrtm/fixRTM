1724eeb909f9918845603b6fe9d0f2b459fe46fb
anatawa12 <anatawa12@icloud.com>
2021-08-15 13:18:17 +0900

    remove unused dependencies from buildSrc and use implementation

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,13 +13,7 @@ repositories {
 }
 
 dependencies {
-    //compile("com.github.TheRandomLabs:CurseAPI:master-SNAPSHOT")
-    compile("org.ow2.asm:asm-commons:6.0")
-    compile("org.ow2.asm:asm-util:6.0")
-    compile("org.ow2.asm:asm:6.0")
-
-    compile("io.sigpipe:jbsdiff:1.0")
-
-    // https://mvnrepository.com/artifact/io.github.java-diff-utils/java-diff-utils
-    implementation("io.github.java-diff-utils:java-diff-utils:4.5")
+    implementation("org.ow2.asm:asm-commons:6.0")
+    implementation("org.ow2.asm:asm-util:6.0")
+    implementation("org.ow2.asm:asm:6.0")
 }
