7f86331203a55aed2f3e74677cdf48cc31a0f5e4
anatawa12 <anatawa12@icloud.com>
2021-10-10 09:28:32 +0900

    Fix version of jar in jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
     id("com.anatawa12.mod-patching.source") version "2.0.1"
     id("com.anatawa12.mod-patching.resources-dev") version "2.0.1"
     id("com.github.johnrengelman.shadow") version "6.1.0"
-    id("com.anatawa12.jarInJar") version "1.0.２"
+    id("com.anatawa12.jarInJar") version "1.0.2"
 }
 
 version = property("modVersion")!!
