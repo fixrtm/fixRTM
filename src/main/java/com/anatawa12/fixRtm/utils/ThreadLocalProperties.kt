cb900b9303bfe32e0739414146390acf0dc183ab
anatawa12 <anatawa12@icloud.com>
2021-11-05 11:56:04 +0900

    fix license year

--- src/main/java/com/anatawa12/fixRtm/utils/ThreadLocalProperties.kt
+++ src/main/java/com/anatawa12/fixRtm/utils/ThreadLocalProperties.kt
@@ -1,4 +1,4 @@
-/// Copyright (c) 2019 anatawa12 and other contributors
+/// Copyright (c) 2021 anatawa12 and other contributors
 /// This file is/was part of fixRTM, released under GNU LGPL v3 with few exceptions
 /// See LICENSE at https://github.com/fixrtm/fixRTM for more details
 
