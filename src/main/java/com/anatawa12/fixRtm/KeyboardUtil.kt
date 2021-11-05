5d52f32749c39e951c1b9e62e4b8d50a1cc6be90
anatawa12 <anatawa12@icloud.com>
2021-11-05 19:02:13 +0900

    remove is/was

--- src/main/java/com/anatawa12/fixRtm/KeyboardUtil.kt
+++ src/main/java/com/anatawa12/fixRtm/KeyboardUtil.kt
@@ -1,5 +1,5 @@
 /// Copyright (c) 2021 anatawa12 and other contributors
-/// This file is/was part of fixRTM, released under GNU LGPL v3 with few exceptions
+/// This file is part of fixRTM, released under GNU LGPL v3 with few exceptions
 /// See LICENSE at https://github.com/fixrtm/fixRTM for more details
 
 package com.anatawa12.fixRtm
--- src/main/java/com/anatawa12/fixRtm/utils/ThreadLocalProperties.kt
+++ src/main/java/com/anatawa12/fixRtm/utils/ThreadLocalProperties.kt
@@ -1,5 +1,5 @@
 /// Copyright (c) 2021 anatawa12 and other contributors
-/// This file is/was part of fixRTM, released under GNU LGPL v3 with few exceptions
+/// This file is part of fixRTM, released under GNU LGPL v3 with few exceptions
 /// See LICENSE at https://github.com/fixrtm/fixRTM for more details
 
 package com.anatawa12.fixRtm.utils
@@ -13,7 +13,7 @@ import kotlin.concurrent.getOrSet
 
 /**
  * The Properties which has ability to have some threads and thier children threads specific properties.
- * Because this uses [InheritableThreadLocal], if you specify a property as thread local, 
+ * Because this uses [InheritableThreadLocal], if you specify a property as thread local,
  * child threads of your current thread will use thread local property.
  */
 class ThreadLocalProperties : Properties() {
