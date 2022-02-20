19e1c4674d99def1a8fa6ea314e2274dc72aafc1
anatawa12 <anatawa12@icloud.com>
2022-02-20 19:01:08 +0900

    add license haeder

--- src/main/kotlin/jp/ngt/rtm/command/CommandRTM.kt
+++ src/main/kotlin/jp/ngt/rtm/command/CommandRTM.kt
@@ -1,3 +1,7 @@
+/// Copyright (c) 2022 anatawa12 and other contributors
+/// This file is part of fixRTM, released under GNU LGPL v3 with few exceptions
+/// See LICENSE at https://github.com/fixrtm/fixRTM for more details
+
 package jp.ngt.rtm.command
 
 import jp.ngt.rtm.RTMResource
