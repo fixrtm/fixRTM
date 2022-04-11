2a7a8becdbd415898e949b2a71236944c499225d
anatawa12 <anatawa12@icloud.com>
2022-04-11 13:09:22 +0900

    add license header

--- src/main/java/com/anatawa12/fixRtm/rtm/item/ItemWithModelEx.kt
+++ src/main/java/com/anatawa12/fixRtm/rtm/item/ItemWithModelEx.kt
@@ -1,3 +1,7 @@
+/// Copyright (c) 2022 anatawa12 and other contributors
+/// This file is part of fixRTM, released under GNU LGPL v3 with few exceptions
+/// See LICENSE at https://github.com/fixrtm/fixRTM for more details
+
 package com.anatawa12.fixRtm.rtm.item
 
 import jp.ngt.ngtlib.block.TileEntityPlaceable
